d878a708709889516860acfa6e30ee01ec20a6ac
Alex Connolly <alex.connolly@linnsystems.com>
2017-02-03 12:28:34 +0000

    why is id here

--- ThemesForLinnworks.js
+++ ThemesForLinnworks.js
@@ -10,7 +10,6 @@ settingsService.getSettings(["ThemesForLinnworks"], function(event)
 	
 	var head  = document.getElementsByTagName('head')[0];
     var link  = document.createElement('link');
-    link.id   = cssId;
     link.rel  = 'stylesheet';
     link.type = 'text/css';
     link.href = url;
