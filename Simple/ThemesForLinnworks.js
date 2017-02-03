67b523b226f13f007e87c5d5098e076fa53d0aa0
Alex Connolly <alex.connolly@linnsystems.com>
2017-02-03 11:41:31 +0000

    ccc

--- Simple/ThemesForLinnworks.js
+++ Simple/ThemesForLinnworks.js
@@ -22,7 +22,7 @@ function ThemesForLinnworksModule($scope, $element, $q) {
 					console.log("error saving theme url");
 				}
 				
-				console.log("complete");
+				console.log("completes");
 		});
 	}
 	
