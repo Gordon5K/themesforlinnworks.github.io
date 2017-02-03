bec69b94b94b026bb4e8ec615013f29070c0a1b8
Alex Connolly <alex.connolly@linnsystems.com>
2017-02-03 12:24:42 +0000

    changes

---
+++ ThemesForLinnworks.js
@@ -0,0 +1,18 @@
+settingsService.getSettings(["ThemesForLinnworks"], function(event)
+{
+	var result = event.result["ThemesForLinnworks"];
+	
+	var setting = result.firstOrDefault("Setting", "ThemeUrl");
+	
+	var url = "https://alexconnolly.github.io/themesforlinnworks.github.io/Styles/" + setting.Value;
+	
+	var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    link.media = 'all';
+	
+    head.appendChild(link);
+});
